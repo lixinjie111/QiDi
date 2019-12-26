9ddd178e49a1ce645bb066eafb5e28662e345692
shilu <shilu@tusvn.comm>
2019-12-26 11:56:23 +0800

    优化

--- static/map3d/utils/PerceptionCars.js
+++ static/map3d/utils/PerceptionCars.js
@@ -540,7 +540,7 @@ class PerceptionCars {
     let s = d.speed.toFixed(1);
     let veh = d.vehicleId.substr(0, 4);
     // let text = "[" + h + ", " + s + ", " + veh + "]";
-    let text = "[" + veh + ", " + s + "°]";
+    let text = "[" + veh + ", " + h + "°]";
     let entityLabel = this.viewer.entities.add({
       id: d.vehicleId + name,
       position: position,
@@ -572,7 +572,7 @@ class PerceptionCars {
     let s = d.speed.toFixed(1);
     let veh = d.vehicleId.substr(0, 4);
     // let text = "[" + h + ", " + s + ", " + veh + "]";
-    let text = "[" + veh + ", " + s + "°]";
+    let text = "[" + veh + ", " + h + "°]";
     carlabel.show = true;
     carlabel.label.text = text;
   }
